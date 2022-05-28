c9f5c25ba38c2cf1ab7d8736527f17f6ea19fc01
LapasaranMarkClarence <105403851+LapasaranMarkClarence@users.noreply.github.com>
2022-05-28 03:54:10 -0700

    Mark part (GUI, function, icon)
    It is the GUI, the function for the button in the loading/introduction screen, and the icon for our stopwatch.

--- stopclock.py
+++ stopclock.py
@@ -27,3 +27,65 @@ progress=Progressbar(loading,style="red.Horizontal.TProgressbar",
                     orient=HORIZONTAL,
                     length=500,
                     mode='determinate',)
+
+
+def stopwatch_GUI():
+    global location
+    stopwatch = Tk()
+    stopwatch.title('Stopwatch')
+    stopwatch.geometry('500x500')
+    
+
+    icon = PhotoImage(file=f'{location}\\icon.png')
+    stopwatch.iconphoto(False, icon)
+    stopwatch.config(background="#a0dfe6")
+    
+
+    photo = PhotoImage(file=f'{location}\\background.png')
+
+    pic = Canvas(stopwatch,
+            width=500, 
+            height=500,
+            bg='black', 
+            relief=RAISED, 
+            bd=10,)
+
+    
+    pic.pack(padx=0,pady=0, 
+        expand=True)  
+    pic.create_image(0,0, 
+        image = photo, 
+        anchor = "nw",)
+    pic.create_text(150,30, 
+                text="4k03h L4n6 703H's Stopwatch", 
+                font=("Arial", 14), 
+                fill="white")
+    
+
+    stopwatch.mainloop()
+
+
+
+def run():
+
+    label_run=Label(loading,text='Loading...',fg='white',bg=frame_bg)
+    Label4_font=('Calibri (Body)',10)
+    label_run.config(font=Label4_font)
+    label_run.place(x=18,y=210)
+    
+    import time
+    r=0
+    for i in range(100):
+        progress['value']=r
+        loading.update_idletasks()
+        time.sleep(0.03)
+        r=r+1
+    
+    loading.destroy()
+    stopwatch_GUI()
+        
+    
+progress.place(x=-10,y=235)
+
+def exit():
+    loading.destroy()
